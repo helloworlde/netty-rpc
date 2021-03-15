a8dfc0c7a76c8fa7122e492d9b9622518269aae2
helloworlde <hellowoodes@gmail.com>
2021-03-15 11:33:27 +0800

    Fixed issue of message encode and decode

--- rpc/src/main/java/io/github/helloworlde/netty/rpc/codec/MessageDecoder.java
+++ rpc/src/main/java/io/github/helloworlde/netty/rpc/codec/MessageDecoder.java
@@ -1,6 +1,8 @@
 package io.github.helloworlde.netty.rpc.codec;
 
+import io.github.helloworlde.netty.rpc.model.MessageType;
 import io.github.helloworlde.netty.rpc.model.Request;
+import io.github.helloworlde.netty.rpc.model.Response;
 import io.github.helloworlde.netty.rpc.serialize.Serialize;
 import io.github.helloworlde.netty.rpc.serialize.SerializeEnum;
 import io.github.helloworlde.netty.rpc.util.Constants;
@@ -14,24 +16,36 @@ import java.util.List;
 @Slf4j
 public class MessageDecoder extends ByteToMessageDecoder {
 
+    /**
+     * Protocol: MagicNumber + MessageType + Serialize + Length + Body
+     */
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        // MagicNumber + Serialize + Length + Body
         int protocol = in.readInt();
         if (Constants.PROTOCOL_MAGIC != protocol) {
             log.info("协议不正确");
             ctx.close();
         }
 
+        // 消息类型
+        int messageType = in.readInt();
+
+        // 序列化类型
         int serializeType = in.readInt();
         Serialize serialize = SerializeEnum.getById(serializeType);
 
+        // Body
         int length = in.readInt();
         byte[] bodyBytes = new byte[length];
         in.readBytes(bodyBytes);
 
-        Request request = serialize.deserialize(bodyBytes, Request.class);
-
-        out.add(request);
+        MessageType messageTypeValue = MessageType.fromType(messageType);
+        if (MessageType.REQUEST.equals(messageTypeValue)) {
+            Request request = serialize.deserialize(bodyBytes, Request.class);
+            out.add(request);
+        } else if (MessageType.RESPONSE.equals(messageTypeValue)) {
+            Response response = serialize.deserialize(bodyBytes, Response.class);
+            out.add(response);
+        }
     }
 }
--- rpc/src/main/java/io/github/helloworlde/netty/rpc/codec/MessageEncoder.java
+++ rpc/src/main/java/io/github/helloworlde/netty/rpc/codec/MessageEncoder.java
@@ -1,24 +1,44 @@
 package io.github.helloworlde.netty.rpc.codec;
 
+import io.github.helloworlde.netty.rpc.model.MessageType;
 import io.github.helloworlde.netty.rpc.model.Request;
+import io.github.helloworlde.netty.rpc.model.Response;
 import io.github.helloworlde.netty.rpc.serialize.Serialize;
 import io.github.helloworlde.netty.rpc.serialize.SerializeEnum;
 import io.github.helloworlde.netty.rpc.util.Constants;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import lombok.extern.slf4j.Slf4j;
 
-public class MessageEncoder extends MessageToByteEncoder<Request> {
+@Slf4j
+public class MessageEncoder extends MessageToByteEncoder<Object> {
 
+    /**
+     * Protocol: MagicNumber + MessageType + Serialize + Length + Body
+     */
     @Override
-    protected void encode(ChannelHandlerContext ctx, Request request, ByteBuf out) throws Exception {
-        // MagicNumber + Serialize + Length + Body
+    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
         out.writeInt(Constants.PROTOCOL_MAGIC);
 
-        Serialize serialize = SerializeEnum.JSON.getSerialize();
-        byte[] requestBody = serialize.serialize(request);
+        // 消息类型
+        MessageType messageType;
+        if (msg instanceof Request) {
+            messageType = MessageType.REQUEST;
+        } else if (msg instanceof Response) {
+            messageType = MessageType.RESPONSE;
+        } else {
+            throw new IllegalArgumentException("Unknown message type");
+        }
 
+        out.writeInt(messageType.getType());
+
+        // 序列化类型
+        Serialize serialize = SerializeEnum.JSON.getSerialize();
         out.writeInt(SerializeEnum.JSON.getId());
+
+        // Body
+        byte[] requestBody = serialize.serialize(msg);
         out.writeInt(requestBody.length);
         out.writeBytes(requestBody);
     }
