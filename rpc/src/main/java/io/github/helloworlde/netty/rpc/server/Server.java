e657f23315c102e429604ad70fd7ee0f88aa1c40
helloworlde <hellowoodes@gmail.com>
2021-03-15 08:31:21 +0800

    Fixed error of Server option

--- rpc/src/main/java/io/github/helloworlde/netty/rpc/server/Server.java
+++ rpc/src/main/java/io/github/helloworlde/netty/rpc/server/Server.java
@@ -8,7 +8,6 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -65,7 +64,6 @@ public class Server {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup)
                            .channel(NioServerSocketChannel.class)
-                           .option(ChannelOption.SO_KEEPALIVE, true)
                            .handler(new LoggingHandler(LogLevel.INFO))
                            .childHandler(new ChannelInitializer<SocketChannel>() {
                                @Override
