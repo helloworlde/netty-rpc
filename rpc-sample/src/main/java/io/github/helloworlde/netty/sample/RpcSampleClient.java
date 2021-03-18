c95a0508aad78ec240114be21facec9a0f1f0b5e
helloworlde <hellowoodes@gmail.com>
2021-03-18 16:27:40 +0800

    Refactor sample

--- rpc-sample/src/main/java/io/github/helloworlde/netty/sample/RpcSampleClient.java
+++ rpc-sample/src/main/java/io/github/helloworlde/netty/sample/RpcSampleClient.java
@@ -5,22 +5,55 @@ import io.github.helloworlde.netty.rpc.client.ServiceProxy;
 import io.github.helloworlde.netty.sample.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 public class RpcSampleClient {
+    static AtomicInteger counter = new AtomicInteger();
 
     public static void main(String[] args) {
+        Client client = null;
         try {
-            Client client = new Client().forAddress("127.0.0.1", 9091)
-                                        .start();
+            client = new Client().forAddress("127.0.0.1", 9091)
+                                 .start();
+
             HelloService helloService = new ServiceProxy<HelloService>(client).newProxy(HelloService.class);
 
             log.info("Client 启动完成");
-            String response = helloService.sayHello("啊哈啊啊啊啊");
+            String response = helloService.sayHello("啊哈啊啊啊啊 " + counter.getAndIncrement());
             log.info("返回的响应结果: {}", response);
 
-            client.shutdown();
+            multiple(helloService);
+
         } catch (Exception e) {
-            e.printStackTrace();
+            log.info("异常: {}", e.getMessage(), e);
+        }
+    }
+
+    private static void multiple(HelloService helloService) throws InterruptedException, java.util.concurrent.ExecutionException {
+
+        List<CompletableFuture<String>> futureList = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+                log.info("发送请求: {}", counter.getAndIncrement());
+                String response = null;
+                try {
+                    response = helloService.sayHello("" + counter.get());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return response;
+            });
+
+        }
+
+        for (CompletableFuture<String> future : futureList) {
+            String response = future.get();
+            log.info("返回的响应结果: {}", response);
         }
     }
 }
--- rpc-sample/src/main/java/io/github/helloworlde/netty/sample/service/impl/HelloServiceImpl.java
+++ rpc-sample/src/main/java/io/github/helloworlde/netty/sample/service/impl/HelloServiceImpl.java
@@ -1,13 +1,29 @@
 package io.github.helloworlde.netty.sample.service.impl;
 
+import io.github.helloworlde.netty.rpc.error.RpcException;
 import io.github.helloworlde.netty.sample.service.HelloService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
 
 @Slf4j
 public class HelloServiceImpl implements HelloService {
+
+    private final Random random = new Random();
+
+    @SneakyThrows
     @Override
     public String sayHello(String message) {
         log.info("新的请求: {}", message);
+        int value = random.nextInt(1000);
+        if (value > 900) {
+            throw new RpcException("业务逻辑异常");
+        } else if (value > 800) {
+            throw new IllegalArgumentException("参数异常");
+        } else if (value > 500) {
+            Thread.sleep(value);
+        }
         return "Hello " + message;
     }
 }
