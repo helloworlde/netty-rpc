445a668986504b8577fe0308eea283fafefd3ead
helloworlde <hellowoodes@gmail.com>
2021-03-22 11:28:52 +0800

    Refactor server sample for register

--- rpc-sample/src/main/java/io/github/helloworlde/netty/sample/RpcSampleServer.java
+++ rpc-sample/src/main/java/io/github/helloworlde/netty/sample/RpcSampleServer.java
@@ -1,5 +1,6 @@
 package io.github.helloworlde.netty.sample;
 
+import io.github.helloworlde.netty.rpc.registry.ConsulRegistry;
 import io.github.helloworlde.netty.rpc.server.Server;
 import io.github.helloworlde.netty.sample.service.HelloService;
 import io.github.helloworlde.netty.sample.service.impl.HelloServiceImpl;
@@ -9,9 +10,17 @@ import lombok.extern.slf4j.Slf4j;
 public class RpcSampleServer {
 
     public static void main(String[] args) throws InterruptedException {
-        Server.server()
-              .port(9091)
-              .addService(HelloService.class, new HelloServiceImpl())
-              .start();
+        Server server = Server.server()
+                              .address("172.30.78.154")
+                              .port(9096)
+                              .name("RPC_SERVER")
+                              .registry(new ConsulRegistry("localhost", 8500))
+                              .addMetadata("env", "test")
+                              .addMetadata("version", "1.0.0")
+                              .addService(HelloService.class, new HelloServiceImpl());
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
     }
 }
