f4d0b3ef8f9217338aedcfa4fc0401ed2d559f2c
helloworlde <hellowoodes@gmail.com>
2021-03-26 14:54:04 +0800

    Add log when service register

--- rpc/rpc-server/src/main/java/io/github/helloworlde/netty/rpc/server/handler/ServiceRegistry.java
+++ rpc/rpc-server/src/main/java/io/github/helloworlde/netty/rpc/server/handler/ServiceRegistry.java
@@ -2,6 +2,7 @@ package io.github.helloworlde.netty.rpc.server.handler;
 
 import io.github.helloworlde.netty.rpc.error.RpcException;
 import io.github.helloworlde.netty.rpc.model.ServiceDefinition;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -10,11 +11,13 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class ServiceRegistry {
 
     private final Map<String, ServiceDefinition<?>> serviceDefinitionMap = new ConcurrentHashMap<>();
 
     public void addService(Class<?> service, Object instance) {
+        log.info("添加服务: {}, 实例: {}", service.getName(), instance.toString());
         serviceDefinitionMap.putIfAbsent(service.getName(), ServiceDefinition.builder()
                                                                              .service(service)
                                                                              .instance(instance)
