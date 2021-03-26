b79c0cf3727470c37aed8395e2e2d7ebd0d9f5af
helloworlde <hellowoodes@gmail.com>
2021-03-26 14:53:28 +0800

    Disable timeout handler for heartbeat

--- rpc/rpc-client/src/main/java/io/github/helloworlde/netty/rpc/client/transport/ClientChannelInitializer.java
+++ rpc/rpc-client/src/main/java/io/github/helloworlde/netty/rpc/client/transport/ClientChannelInitializer.java
@@ -7,8 +7,6 @@ import io.github.helloworlde.netty.rpc.model.Response;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 
 public class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -24,8 +22,8 @@ public class ClientChannelInitializer extends ChannelInitializer<SocketChannel> 
         // lengthFieldLength 请求内容的长度标识偏移量 Length = 4
         ch.pipeline()
           .addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 8, 4))
-          .addLast(new ReadTimeoutHandler(10))
-          .addLast(new WriteTimeoutHandler(10))
+          // .addLast(new ReadTimeoutHandler(10))
+          // .addLast(new WriteTimeoutHandler(10))
           .addLast(new MessageEncoder())
           .addLast(new MessageDecoder<>(Response.class))
           .addLast(handler);
