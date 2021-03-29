a34972ef02e00943b8de52aa03e411d857700a1b
helloworlde <hellowoodes@gmail.com>
2021-03-29 10:58:46 +0800

    Refactor public configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,11 @@ plugins {
     java
     idea
     id("io.freefair.lombok") version "5.3.0"
-    `maven-publish`
 }
 
 subprojects {
     apply(plugin = "java")
     apply(plugin = "application")
-    apply(plugin = "maven-publish")
     apply(plugin = "io.freefair.lombok")
 
     group = "io.github.helloworlde"
@@ -51,58 +49,5 @@ subprojects {
     java {
         withJavadocJar()
         withSourcesJar()
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-                versionMapping {
-                    usage("java-api") {
-                        fromResolutionOf("runtimeClasspath")
-                    }
-                    usage("java-runtime") {
-                        fromResolutionResult()
-                    }
-                }
-                pom {
-                    description.set("Netty RPC Core")
-                    url.set("https://maven.pkg.github.com/helloworlde/netty-rpc")
-                    properties.set(
-                        mapOf(
-                            "myProp" to "value",
-                            "prop.with.dots" to "anotherValue"
-                        )
-                    )
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("helloworlde")
-                            name.set("helloworlde")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/helloworlde/netty-rpc.git")
-                        developerConnection.set("scm:git:ssh://github.com/helloworlde/netty-rpc.git")
-                        url.set("http://github.com/helloworlde/netty-rpc")
-                    }
-                }
-            }
-        }
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/helloworlde/netty-rpc")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
-            }
-        }
     }
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,1 +1,60 @@
-val name = "netty-rpc-core"
+plugins {
+    `maven-publish`
+}
+apply(plugin = "maven-publish")
+
+base.archivesBaseName = "netty-rpc-core"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = tasks.jar.get().archiveBaseName.get()
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                description.set("Netty RPC Core")
+                url.set("https://maven.pkg.github.com/helloworlde/netty-rpc")
+                properties.set(
+                    mapOf(
+                        "myProp" to "value",
+                        "prop.with.dots" to "anotherValue"
+                    )
+                )
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("helloworlde")
+                        name.set("helloworlde")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/helloworlde/netty-rpc.git")
+                    developerConnection.set("scm:git:ssh://github.com/helloworlde/netty-rpc.git")
+                    url.set("http://github.com/helloworlde/netty-rpc")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/helloworlde/netty-rpc")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
