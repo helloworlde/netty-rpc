513e5b0497da852ea5b74004d403affd911be27f
helloworlde <hellowoodes@gmail.com>
2021-03-31 10:34:08 +0800

    Build client in example for refactor

--- example/helloworld/src/main/java/io/github/helloworlde/netty/helloworld/HelloworldClient.java
+++ example/helloworld/src/main/java/io/github/helloworlde/netty/helloworld/HelloworldClient.java
@@ -1,6 +1,7 @@
 package io.github.helloworlde.netty.helloworld;
 
 import io.github.helloworlde.netty.rpc.client.Client;
+import io.github.helloworlde.netty.rpc.client.ClientBuilder;
 import io.github.helloworlde.netty.rpc.client.proxy.ServiceProxy;
 import io.github.helloworlde.netty.rpc.example.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 public class HelloworldClient {
 
     public static void main(String[] args) throws Exception {
-        Client client = Client.client()
-                              .forAddress("127.0.0.1", 9096)
-                              .start();
+        Client client = ClientBuilder.builder()
+                                     .forAddress("127.0.0.1", 9096)
+                                     .build();
+
+        client.start();
 
         HelloService helloService = new ServiceProxy(client).newProxy(HelloService.class);
 
--- example/register/src/main/java/io/github/helloworlde/netty/register/RegisterClient.java
+++ example/register/src/main/java/io/github/helloworlde/netty/register/RegisterClient.java
@@ -1,7 +1,7 @@
 package io.github.helloworlde.netty.register;
 
 import io.github.helloworlde.netty.rpc.client.Client;
-import io.github.helloworlde.netty.rpc.client.lb.RoundRobinLoadBalancer;
+import io.github.helloworlde.netty.rpc.client.ClientBuilder;
 import io.github.helloworlde.netty.rpc.client.nameresovler.ConsulNameResolver;
 import io.github.helloworlde.netty.rpc.client.proxy.ServiceProxy;
 import io.github.helloworlde.netty.rpc.example.service.HelloService;
@@ -11,11 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 public class RegisterClient {
 
     public static void main(String[] args) throws Exception {
-        Client client = Client.client()
-                              .forTarget("netty-rpc-server")
-                              .nameResolver(new ConsulNameResolver("127.0.0.1", 8500))
-                              .loadBalancer(new RoundRobinLoadBalancer())
-                              .start();
+        Client client = ClientBuilder.builder()
+                                     .forTarget("netty-rpc-server")
+                                     .nameResolver(new ConsulNameResolver("127.0.0.1", 8500))
+                                     .build();
+
+        client.start();
 
         HelloService helloService = new ServiceProxy(client).newProxy(HelloService.class);
 
