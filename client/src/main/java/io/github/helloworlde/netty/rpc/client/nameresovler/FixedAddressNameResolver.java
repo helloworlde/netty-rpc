be43cbb05aa631615b0361b801af366deceda120
helloworlde <hellowoodes@gmail.com>
2021-04-01 17:44:22 +0800

    Fixed issue address can't be null when FixedAddressNameResolver init

--- client/src/main/java/io/github/helloworlde/netty/rpc/client/nameresovler/FixedAddressNameResolver.java
+++ client/src/main/java/io/github/helloworlde/netty/rpc/client/nameresovler/FixedAddressNameResolver.java
@@ -3,6 +3,7 @@ package io.github.helloworlde.netty.rpc.client.nameresovler;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,9 +12,13 @@ public class FixedAddressNameResolver extends NameResolver {
     private List<SocketAddress> addresses;
 
     public FixedAddressNameResolver(List<ServerAddress> addresses) {
-        this.addresses = addresses.stream()
-                                  .map(address -> new InetSocketAddress(address.getHostname(), address.getPort()))
-                                  .collect(Collectors.toList());
+        if (addresses == null) {
+            this.addresses = Collections.emptyList();
+        } else {
+            this.addresses = addresses.stream()
+                                      .map(address -> new InetSocketAddress(address.getHostname(), address.getPort()))
+                                      .collect(Collectors.toList());
+        }
     }
 
     public FixedAddressNameResolver(SocketAddress... addresses) {
