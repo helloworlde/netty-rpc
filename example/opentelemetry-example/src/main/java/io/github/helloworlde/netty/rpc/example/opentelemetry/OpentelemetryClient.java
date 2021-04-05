d45df2392bd30cdf99649cc080a9f38996a4e13c
helloworlde <hellowoodes@gmail.com>
2021-04-05 14:56:36 +0800

    Add client span for example

--- example/opentelemetry-example/src/main/java/io/github/helloworlde/netty/rpc/example/opentelemetry/OpentelemetryClient.java
+++ example/opentelemetry-example/src/main/java/io/github/helloworlde/netty/rpc/example/opentelemetry/OpentelemetryClient.java
@@ -8,6 +8,8 @@ import io.github.helloworlde.netty.rpc.client.ClientBuilder;
 import io.github.helloworlde.netty.rpc.client.proxy.ServiceProxy;
 import io.github.helloworlde.netty.rpc.example.service.HelloService;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,18 +19,30 @@ public class OpentelemetryClient {
 
         OpenTelemetry telemetry = OpenTelemetryConfig.getOpenTelemetry(ExporterEnum.Jaeger, "客户端", "127.0.0.1", 14250);
         // OpenTelemetry telemetry = OpenTelemetryConfig.getOpenTelemetry(ExporterEnum.Zipkin, "客户端", "127.0.0.1", 9411);
+        final Span span = telemetry.getTracer("NETTY_RPC")
+                                   .spanBuilder("ClientStart")
+                                   .setNoParent()
+                                   .startSpan();
 
-        Client client = ClientBuilder.builder()
-                                     .forAddress("127.0.0.1", 9096)
-                                     .addInterceptor(new ClientTraceInterceptor(telemetry))
-                                     .build();
+        Client client = null;
+        try (final Scope scope = span.makeCurrent()) {
+            client = ClientBuilder.builder()
+                                  .forAddress("127.0.0.1", 9096)
+                                  .addInterceptor(new ClientTraceInterceptor(telemetry))
+                                  .build();
 
-        client.start();
+            client.start();
 
-        HelloService helloService = new ServiceProxy(client).newProxy(HelloService.class);
 
-        String response = helloService.sayHello("Netty RPC");
-        log.info("返回的响应结果: {}", response);
+            HelloService helloService = new ServiceProxy(client).newProxy(HelloService.class);
+
+            String response = helloService.sayHello("Netty RPC");
+            log.info("返回的响应结果: {}", response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            span.end();
+        }
 
         client.shutdown();
     }
