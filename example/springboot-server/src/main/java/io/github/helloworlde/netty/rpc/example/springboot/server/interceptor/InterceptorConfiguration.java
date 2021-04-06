508972be5475b46ca5787b451e2030e58290ce6a
helloworlde <hellowoodes@gmail.com>
2021-04-06 20:08:27 +0800

    Add opentelemetry configuration for example

--- example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/interceptor/InterceptorConfiguration.java
+++ example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/interceptor/InterceptorConfiguration.java
@@ -1,12 +1,30 @@
 package io.github.helloworlde.netty.rpc.example.springboot.server.interceptor;
 
+import io.github.helloworlde.netty.opentelemetry.OpenTelemetryConfig;
 import io.github.helloworlde.netty.opentelemetry.metrics.ServerMetricsInterceptor;
+import io.github.helloworlde.netty.opentelemetry.trace.config.ExporterEnum;
+import io.github.helloworlde.netty.opentelemetry.trace.server.ServerTraceInterceptor;
+import io.opentelemetry.api.OpenTelemetry;
 import io.prometheus.client.CollectorRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InterceptorConfiguration {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${opentelemetry.exporter.name}")
+    private ExporterEnum exporter;
+
+    @Value("${opentelemetry.exporter.address}")
+    private String exporterAddress;
+
+    @Value("${opentelemetry.exporter.port}")
+    private Integer exporterPort;
+
 
     @Bean
     public CustomServerInterceptor clientInterceptor() {
@@ -16,5 +34,11 @@ public class InterceptorConfiguration {
     @Bean
     public ServerMetricsInterceptor serverMetricsInterceptor(CollectorRegistry collectorRegistry) {
         return new ServerMetricsInterceptor(collectorRegistry);
+    }
+
+    @Bean
+    public ServerTraceInterceptor clientTraceInterceptor() {
+        OpenTelemetry openTelemetry = OpenTelemetryConfig.getOpenTelemetry(exporter, applicationName, exporterAddress, exporterPort);
+        return new ServerTraceInterceptor(openTelemetry);
     }
 }
