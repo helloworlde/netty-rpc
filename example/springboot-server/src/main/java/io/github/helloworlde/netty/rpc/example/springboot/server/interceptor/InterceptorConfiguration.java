73375198f2088b7c311aeee73652a7d404de4310
helloworlde <hellowoodes@gmail.com>
2021-04-06 17:53:15 +0800

    Add server interceptor for example

--- example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/interceptor/InterceptorConfiguration.java
+++ example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/interceptor/InterceptorConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.helloworlde.netty.rpc.example.springboot.server.interceptor;
 
+import io.github.helloworlde.netty.opentelemetry.metrics.ServerMetricsInterceptor;
+import io.prometheus.client.CollectorRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +11,10 @@ public class InterceptorConfiguration {
     @Bean
     public CustomServerInterceptor clientInterceptor() {
         return new CustomServerInterceptor();
+    }
+
+    @Bean
+    public ServerMetricsInterceptor serverMetricsInterceptor(CollectorRegistry collectorRegistry) {
+        return new ServerMetricsInterceptor(collectorRegistry);
     }
 }
