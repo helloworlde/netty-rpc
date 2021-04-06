b44f4cdc6cc210598f77465298eea1eb3bb915b0
helloworlde <hellowoodes@gmail.com>
2021-04-06 21:00:32 +0800

    Add junit test for opentelemetry spring boot starter

---
+++ spring-boot-starter-opentelemetry/src/test/java/io/github/helloworlde/netty/rpc/opentelemetry/server/NettyRpcStarterServer.java
@@ -0,0 +1,13 @@
+package io.github.helloworlde.netty.rpc.opentelemetry.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyRpcStarterServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRpcStarterServer.class);
+    }
+
+}
