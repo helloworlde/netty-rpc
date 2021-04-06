8d730907074806f9d5687ed6dc5213ef458bd611
helloworlde <hellowoodes@gmail.com>
2021-04-06 17:35:28 +0800

    Completed client request statistics

--- opentelemetry/src/main/java/io/github/helloworlde/netty/opentelemetry/metrics/ClientMetricsInterceptor.java
+++ opentelemetry/src/main/java/io/github/helloworlde/netty/opentelemetry/metrics/ClientMetricsInterceptor.java
@@ -6,189 +6,66 @@ import io.github.helloworlde.netty.rpc.interceptor.ClientInterceptor;
 import io.github.helloworlde.netty.rpc.model.Request;
 import io.opentelemetry.api.metrics.DoubleValueRecorder;
 import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.LongValueObserver;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.metrics.common.Labels;
-import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
-import io.opentelemetry.sdk.metrics.aggregator.Aggregator;
-import io.opentelemetry.sdk.metrics.aggregator.AggregatorFactory;
-import io.opentelemetry.sdk.metrics.aggregator.AggregatorHandle;
-import io.opentelemetry.sdk.metrics.common.InstrumentDescriptor;
-import io.opentelemetry.sdk.metrics.common.InstrumentType;
-import io.opentelemetry.sdk.metrics.common.InstrumentValueType;
-import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
-import io.opentelemetry.sdk.metrics.data.DoubleHistogramData;
-import io.opentelemetry.sdk.metrics.data.DoubleHistogramPointData;
-import io.opentelemetry.sdk.metrics.data.MetricData;
-import io.opentelemetry.sdk.resources.Resource;
 import io.prometheus.client.CollectorRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class ClientMetricsInterceptor implements ClientInterceptor {
 
-    private CollectorRegistry collectorRegistry;
+    private final LongCounter requestCountCounter;
 
-    private final AtomicLong requestCounter = new AtomicLong();
-
-    private final LongCounter requestCounterLong;
-
-    private final AggregatorHandle<Object> requestTimeHandler;
-
-    private DoubleHistogramPointData doubleHistogram;
-    private MetricData timeHistogram;
-
-
-    static final String DIMENSION_API_NAME = "apiName";
-    static final String DIMENSION_STATUS_CODE = "statusCode";
-
-    static String API_COUNTER_METRIC = "apiBytesSent";
-    static String API_LATENCY_METRIC = "latency";
-    static String API_SUM_METRIC = "totalApiBytesSent";
-    static String API_LAST_LATENCY_METRIC = "lastLatency";
-    static String API_UP_DOWN_COUNTER_METRIC = "queueSizeChange";
-    static String API_UP_DOWN_SUM_METRIC = "actualQueueSize";
-
-    String apiNameValue = "";
-    String statusCodeValue = "";
-
-    long apiLastLatency = 1;
-    LongValueObserver apiLastLatencyObserver;
-
-    double costTime = 0;
-    DoubleValueRecorder costRecorder;
-
-    final Aggregator<Object> objectAggregator;
-
-    int counter = 0;
-
+    private final DoubleValueRecorder requestTimeRecorder;
 
     public ClientMetricsInterceptor(CollectorRegistry collectorRegistry) {
-        this.collectorRegistry = collectorRegistry;
         MetricsConfiguration metricsConfiguration = new MetricsConfiguration(collectorRegistry);
         Meter meter = metricsConfiguration.getMeter();
 
-        meter.longValueObserverBuilder("netty.rpc.request.count")
-             .setDescription("请求个数")
-             .setUnit("request")
-             .setUpdater(result -> result.observe(requestCounter.get(), Labels.of("role", "client")))
-             .build();
+        requestCountCounter = meter.longCounterBuilder("netty.rpc.request.count")
+                                   .setDescription("请求数量")
+                                   .build();
 
-        requestCounterLong = meter.longCounterBuilder("netty.rpc.request")
-                                  .setDescription("总请求个数")
-                                  .setUnit("个")
-                                  .build();
-
-        AggregatorFactory aggregatorFactory = metricsConfiguration.getAggregatorFactory();
-
-        objectAggregator = aggregatorFactory.create(Resource.getDefault(),
-                InstrumentationLibraryInfo.empty(),
-                InstrumentDescriptor.create(
-                        "netty.rpc.request.time",
-                        "请求耗费时间",
-                        "ms",
-                        InstrumentType.VALUE_RECORDER,
-                        InstrumentValueType.DOUBLE
-                ));
-
-        requestTimeHandler = objectAggregator.createHandle();
-
-        costRecorder = meter.doubleValueRecorderBuilder("netty.rpc.request.cost")
-                            .setDescription("时间哈哈哈哈")
-                            .setUnit("ms")
-                            .build();
-
-        meter.doubleSumObserverBuilder("netty.rpc.request.cost.sum")
-             .setDescription("时间统计")
-             .setUnit("ms")
-             .setUpdater(doubleResult -> {
-                 doubleResult.observe(costTime, Labels.of("time", "request"));
-             });
-
-        //
-        // MetricData doubleHistogram = MetricData.createDoubleHistogram();
-
-
-        DoubleHistogramPointData doubleHistogramPointData = DoubleHistogramPointData.create(
-                0,
-                100,
-                Labels.of("role", "client"),
-                100,
-                Arrays.asList(0.5d, 0.9d, 1d),
-                Arrays.asList(5L, 9L, 99L, 100L)
-        );
-
-        final DoubleHistogramData doubleHistogramData = DoubleHistogramData.create(AggregationTemporality.CUMULATIVE, Arrays.asList(doubleHistogramPointData));
-
-
-        timeHistogram = MetricData.createDoubleHistogram(
-                Resource.getDefault(),
-                InstrumentationLibraryInfo.empty(),
-                "netty.rpc.request.time",
-                "请求耗时",
-                "ms",
-                doubleHistogramData
-        );
-
-
-        apiLastLatencyObserver = meter.longValueObserverBuilder("netty.rpc.request.time")
-                                      .setDescription("请求花费的时间啊啊啊")
-                                      .setUnit("ms")
-                                      .setUpdater(longResult -> {
-                                          longResult.observe(apiLastLatency,
-                                                  Labels.of(DIMENSION_API_NAME,
-                                                          apiNameValue,
-                                                          DIMENSION_STATUS_CODE,
-                                                          statusCodeValue));
-                                      })
-                                      .build();
-
+        requestTimeRecorder = meter.doubleValueRecorderBuilder("netty.rpc.request.duration")
+                                   .setDescription("请求时间")
+                                   .setUnit("ms")
+                                   .build();
     }
 
 
     @Override
     public Object interceptorCall(Request request, CallOptions callOptions, ClientCall next) throws Exception {
-        requestCounter.incrementAndGet();
-
         Instant startTime = Instant.now();
 
-        log.info("当前请求个数: {}", requestCounter.get());
+        String serviceName = request.getServiceName();
+        String methodName = request.getMethodName();
+
         Object result;
         try {
             result = next.call(request, callOptions);
-            requestCounterLong.add(1L, Labels.of("result", "success"));
+            recordRequestCount(serviceName, methodName, "success");
         } catch (Exception e) {
-            requestCounterLong.add(1L, Labels.of("result", "error"));
+            recordRequestCount(serviceName, methodName, "error");
             throw e;
         } finally {
+            recordRequestCount(serviceName, methodName, "sum");
+
             long costTime = Duration.between(startTime, Instant.now()).toMillis();
-            apiLastLatency = costTime;
-            this.costTime = costTime;
-            log.info("花费的时间: {}", costTime);
-            requestTimeHandler.recordDouble(costTime);
-            costRecorder.record(costTime);
-            counter++;
-            if (counter > 99) {
-                MetricData metricData = objectAggregator.toMetricData(
-                        Collections.singletonMap(Labels.empty(), requestTimeHandler.accumulateThenReset()),
-                        0,
-                        10,
-                        100
-                );
-
-                // Object o = requestTimeHandler.accumulateThenReset();
-
-
-                log.info("统计结果: {}", metricData);
-            }
+            requestTimeRecorder.record(costTime);
         }
         return result;
+    }
+
+    private void recordRequestCount(String serviceName, String methodName, String type) {
+        Labels labels = Labels.builder()
+                              .put("service_name", serviceName)
+                              .put("method_name", methodName)
+                              .put("type", type)
+                              .build();
+        requestCountCounter.add(1L, labels);
     }
 
     @Override
--- opentelemetry/src/main/java/io/github/helloworlde/netty/opentelemetry/metrics/MetricsConfiguration.java
+++ opentelemetry/src/main/java/io/github/helloworlde/netty/opentelemetry/metrics/MetricsConfiguration.java
@@ -3,14 +3,7 @@ package io.github.helloworlde.netty.opentelemetry.metrics;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.exporter.prometheus.PrometheusCollector;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.aggregator.AggregatorFactory;
-import io.opentelemetry.sdk.metrics.common.InstrumentType;
-import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
-import io.opentelemetry.sdk.metrics.view.InstrumentSelector;
 import io.prometheus.client.CollectorRegistry;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MetricsConfiguration {
 
@@ -18,39 +11,20 @@ public class MetricsConfiguration {
 
     private final Meter meter;
 
-    private final CollectorRegistry collectorRegistry;
-
     private final PrometheusCollector prometheusCollector;
 
-    private AggregatorFactory aggregatorFactory;
-
     public MetricsConfiguration(CollectorRegistry collectorRegistry) {
-        this.collectorRegistry = collectorRegistry;
 
         this.provider = SdkMeterProvider.builder()
                                         .buildAndRegisterGlobal();
-
-        InstrumentSelector selector = InstrumentSelector.builder()
-                                                        .setInstrumentType(InstrumentType.COUNTER)
-                                                        .build();
-        // AggregatorFactory aggregatorFactory = AggregatorFactory.count(AggregationTemporality.CUMULATIVE);
-        // AggregatorFactory aggregatorFactory = AggregatorFactory.minMaxSumCount();
-
-        // Double[] boundaries = {0.1d, 0.5d, 0.9d, 0.99d, 1d};
-        List<Double> boundaries = Arrays.asList(10d, 20d, 30d, 40d, 50d, 60d, 70d, 80d, 90d, 100d, 500d, 1000d);
-
-        AggregatorFactory aggregatorFactory = AggregatorFactory.histogram(boundaries, AggregationTemporality.DELTA);
-
-        // this.provider.registerView(selector, aggregatorFactory);
-
-        this.aggregatorFactory = aggregatorFactory;
 
         this.meter = provider.get("NETTY_RPC");
 
         this.prometheusCollector = PrometheusCollector.builder()
                                                       .setMetricProducer(provider)
-                                                      .buildAndRegister()
-                                                      .register(collectorRegistry);
+                                                      .buildAndRegister();
+
+        collectorRegistry.register(prometheusCollector);
     }
 
 
@@ -58,8 +32,11 @@ public class MetricsConfiguration {
         return meter;
     }
 
+    public SdkMeterProvider getProvider() {
+        return provider;
+    }
 
-    public AggregatorFactory getAggregatorFactory() {
-        return aggregatorFactory;
+    public PrometheusCollector getPrometheusCollector() {
+        return prometheusCollector;
     }
 }
