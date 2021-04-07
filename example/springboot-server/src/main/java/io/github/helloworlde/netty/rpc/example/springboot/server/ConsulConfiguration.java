9bc80d2a52581b0a995744790a0f7d8d2dcf4c88
helloworlde <hellowoodes@gmail.com>
2021-04-07 08:22:54 +0800

    Disable consul register

--- example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/ConsulConfiguration.java
+++ example/springboot-server/src/main/java/io/github/helloworlde/netty/rpc/example/springboot/server/ConsulConfiguration.java
@@ -4,9 +4,8 @@ package io.github.helloworlde.netty.rpc.example.springboot.server;
 import io.github.helloworlde.netty.rpc.registry.ConsulRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class ConsulConfiguration {
 
     @Value("${consul.host}")
