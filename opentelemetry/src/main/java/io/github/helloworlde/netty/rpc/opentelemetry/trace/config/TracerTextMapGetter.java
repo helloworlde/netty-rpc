b2976382213910adc57004ed216c07a9449a6151
helloworlde <hellowoodes@gmail.com>
2021-04-12 10:55:02 +0800

    Remove no sense log of trace

--- opentelemetry/src/main/java/io/github/helloworlde/netty/rpc/opentelemetry/trace/config/TracerTextMapGetter.java
+++ opentelemetry/src/main/java/io/github/helloworlde/netty/rpc/opentelemetry/trace/config/TracerTextMapGetter.java
@@ -21,7 +21,6 @@ public class TracerTextMapGetter implements TextMapGetter<Map<String, Object>> {
         if (Objects.nonNull(carrier) && carrier.containsKey(key)) {
             return carrier.get(key).toString();
         }
-        log.debug("没有获取到 name: {} 的信息", key);
         return null;
     }
 }
