62dde5b76936266c91305d77d0bf70489f154664
helloworlde <hellowoodes@gmail.com>
2021-04-12 15:27:32 +0800

    Rewrite ResponseFuture get method for more detail when timeout

--- client/src/main/java/io/github/helloworlde/netty/rpc/client/request/ResponseFuture.java
+++ client/src/main/java/io/github/helloworlde/netty/rpc/client/request/ResponseFuture.java
@@ -1,7 +1,21 @@
 package io.github.helloworlde.netty.rpc.client.request;
 
 import io.netty.util.concurrent.DefaultPromise;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
 public class ResponseFuture<V> extends DefaultPromise<V> {
 
+    @Override
+    public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        try {
+            return super.get(timeout, unit);
+        } catch (TimeoutException e) {
+            throw new TimeoutException(String.format("请求超时，超时时间: %d", timeout));
+        }
+    }
 }
